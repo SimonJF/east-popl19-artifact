3637bd37d6168a64d2cf47f276a913677fe9bf1a
Simon Fowler <simon@simonjf.com>
2018-10-12 16:40:39 +0100

    Add examples.

---
+++ links-docker/run_chatserver.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+./links/links --path=examples/chatserver/ --config=config examples/chatserver/chatServer.links
---
+++ links-docker/run_two_factor.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+./links/links --path=examples/two-factor/ --config=config examples/two-factor/twoFactor.links
